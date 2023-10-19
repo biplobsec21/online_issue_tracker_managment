6c7ada23a02407520b7ab396fc5cce616eef6e6f
biplobsec21 <biplobsec21@gmail.com>
2023-10-19 19:31:44 +0600

    Edit page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,11 +1,8 @@
-import prisma from '@/prisma/client'
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import prisma from '@/prisma/client';
+import { Box, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import React from 'react'
-import IssueStatusBadge from '../../components/IssueStatusBadge';
-import ReactMarkdown from 'react-markdown';
-import {Pencil2Icon} from '@radix-ui/react-icons'
-import Link from 'next/link';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 interface Props{
     params:{id:string}
 }
@@ -21,25 +18,10 @@ const IssueDetailPage = async ({params} : Props) => {
   return (
     <Grid columns={{initial:"1",md:"2"}} gap="5">
         <Box>
-            <Heading>{issue.title}</Heading>
-            <Flex my="2" className='space-x-3'>
-                <IssueStatusBadge status={issue.status}/>
-                <Text>{issue.created_at.toDateString()}</Text>
-            </Flex>
-            
-            <Card className="prose">
-                <ReactMarkdown>{issue.description}</ReactMarkdown>
-            </Card>
-            
+            <IssueDetails issue={issue}/>
         </Box>
         <Box>
-            <Button>
-            <Pencil2Icon />
-                <Link href={`/issue/${issue.id}/edit`}>
-                    Edit Issue 
-                </Link>
-            </Button>
-            
+          <EditIssueButton issueId={issue.id}/>
         </Box>
     </Grid>
     
---
+++ app/issues/_components/IssueForm.tsx
@@ -0,0 +1,75 @@
+
+'use client';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+import "easymde/dist/easymde.min.css";
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchema';
+import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
+import dynamic from 'next/dynamic';
+import { Issue } from '@prisma/client';
+
+const SimpleMDE = dynamic(
+    () => import("react-simplemde-editor"),
+    { ssr: false }
+);
+type IssueFormData = z.infer<typeof createIssueSchema>;
+
+const IssueForm = ({ issue }: { issue?: Issue }) => {
+
+    const route = useRouter();
+    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueFormData>({
+        resolver: zodResolver(createIssueSchema)
+    });
+    const [error, setError] = useState('');
+    const [isSubmitting, setSubmitting] = useState(false);
+    const onSubmit = handleSubmit(async (data) => {
+        try {
+            setSubmitting(true);
+            await axios.post('/api/issues', data);
+            route.push('/issues');
+
+        } catch (error) {
+            setSubmitting(false);
+            setError('An unexpected error occured.');
+        }
+
+    });
+    return (
+        <div className='max-w-xl'>
+            {error && <Callout.Root color='red' className='mb-5'>
+                <Callout.Text>
+                    Something went wrong!
+                </Callout.Text>
+            </Callout.Root>}
+            <form
+                className='space-y-4'
+                onSubmit={onSubmit} >
+                <TextField.Root>
+                    <TextField.Input defaultValue={issue?.title} placeholder='Title' {...register('title')} />
+                </TextField.Root>
+                <ErrorMessage>
+                    {errors.title?.message}
+                </ErrorMessage>
+                <Controller
+                    name='description'
+                    control={control}
+                    defaultValue={issue?.description}
+                    render={({ field }) => <SimpleMDE placeholder='Enter description' {...field} />}
+                />
+                <ErrorMessage>
+                    {errors.description?.message}
+                </ErrorMessage>
+                <Button disabled={isSubmitting}>Create new issue {isSubmitting && <Spinner />}</Button>
+            </form>
+        </div>
+
+    )
+}
+
+export default IssueForm
