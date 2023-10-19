ad9d09aab062582e643651f69ad302676b278bee
biplobsec21 <biplobsec21@gmail.com>
2023-10-19 12:12:35 +0600

    Loading page added

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,8 +1,23 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
+import { Heading, Flex, Card, Box } from '@radix-ui/themes'
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const LoadingIssueDetailPage = () => {
   return (
-    <div>Loading..</div>
+    <Box className='max-w-xl'>
+        <Skeleton />
+        <Flex my="2" className='space-x-3'>
+            <Skeleton width="5rem"/>
+            <Skeleton width="8rem"/>
+        </Flex>
+
+        <Card className="prose">
+            <Skeleton count={3}/>
+        </Card>
+    </Box>
   )
 }
 
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,8 +1,15 @@
+import { Box } from '@radix-ui/themes'
 import React from 'react'
-
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 const LoadingNewIssuePage = () => {
   return (
-    <div>loading...</div>
+    <Box className='max-w-xl'>
+        <Skeleton height="4rem" />
+        <Skeleton height="25rem"/>
+        <Skeleton width="5rem"/>
+
+    </Box>
   )
 }
 
