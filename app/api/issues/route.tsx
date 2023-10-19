c59ba9c0ecf58df29f87e646ed4d45c104e1fa06
biplobsec21 <biplobsec21@gmail.com>
2023-10-19 23:30:54 +0600

    Build an api for update

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,16 +1,16 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { createIssueSchema } from "../../validationSchema";
-export async function POST(request: NextRequest){
-    const body= await request.json();
+import { issueSchema } from "../../validationSchema";
+export async function POST(request: NextRequest) {
+    const body = await request.json();
 
-    const validation = createIssueSchema.safeParse(body);
-    if(!validation.success)
-        return NextResponse.json(validation.error.format(),{status:400});
+    const validation = issueSchema.safeParse(body);
+    if (!validation.success)
+        return NextResponse.json(validation.error.format(), { status: 400 });
 
-    const newIssue=await prisma.issue.create({
-        data:{ title: body.title, description: body.description}
+    const newIssue = await prisma.issue.create({
+        data: { title: body.title, description: body.description }
     });
 
-    return NextResponse.json(newIssue,{status:201});
+    return NextResponse.json(newIssue, { status: 201 });
 }
