4f0b7fefc94830ed91e6b93498b03961188f56dd
biplobsec21 <biplobsec21@gmail.com>
2023-10-19 16:55:53 +0600

    Organize defaut export

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,6 +1,5 @@
+import { Skeleton } from '@/app/components'
 import { Table } from '@radix-ui/themes'
-import React from 'react'
-import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import IssueActions from './IssueActions'
 const loading = () => {
