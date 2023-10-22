39bb6b38eddbee105289f92fc5e6e7bc3c7f3299
biplobsec21 <biplobsec21@gmail.com>
2023-10-22 17:14:55 +0600

    Login skeleton added

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 import classnames from 'classnames';
+import { Skeleton } from "@/app/components"
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -53,7 +54,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
 
   const { status, data: session } = useSession();
-  if (status === 'loading') return null;
+  if (status === 'loading') return <Skeleton width="3rem" />;
   if (status === 'unauthenticated')
     return <Link className="nav-link" href="/api/auth/signin"> Login</Link>;
 
