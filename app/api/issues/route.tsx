7d6240b16975919705671a8ec0d0d3b523feab9d
biplobsec21 <biplobsec21@gmail.com>
2023-10-23 11:57:39 +0600

    Secure the application

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,7 +1,12 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { issueSchema } from "../../validationSchema";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 export async function POST(request: NextRequest) {
+    const session = await getServerSession(authOptions);
+    if (!session)
+        return NextResponse.json({}, { status: 401 });
     const body = await request.json();
 
     const validation = issueSchema.safeParse(body);
