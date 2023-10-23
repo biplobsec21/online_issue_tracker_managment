efb37b9c176c9c5f2584479c47a25839ad105512
biplobsec21 <biplobsec21@gmail.com>
2023-10-23 11:57:39 +0600

    Secure the application

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -1,10 +1,15 @@
+import authOptions from "@/app/auth/authOptions";
 import { issueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
     request: NextRequest,
     { params }: { params: { id: string } }) {
+    const session = await getServerSession(authOptions);
+    if (!session)
+        return NextResponse.json({}, { status: 401 });
 
     const body = await request.json();
     const validation = issueSchema.safeParse(body);
@@ -31,6 +36,9 @@ export async function DELETE(
     request: NextRequest,
     { params }: { params: { id: string } }) {
 
+    const session = await getServerSession(authOptions);
+    if (!session)
+        return NextResponse.json({}, { status: 401 });
 
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(params.id) }
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,12 +4,14 @@ import { notFound } from 'next/navigation';
 import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
 import DeleteIssueButton from './DeleteIssueButton';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/auth/authOptions';
 interface Props {
   params: { id: string }
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-
+  const session = await getServerSession(authOptions)
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) }
   });
@@ -21,12 +23,12 @@ const IssueDetailPage = async ({ params }: Props) => {
       <Box className='md:col-span-4'>
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
+      {session && <Box>
         <Flex direction="column" gap="5">
           <EditIssueButton issueId={issue.id} />
           <DeleteIssueButton issueId={issue.id} />
         </Flex>
-      </Box>
+      </Box>}
     </Grid>
 
   )
