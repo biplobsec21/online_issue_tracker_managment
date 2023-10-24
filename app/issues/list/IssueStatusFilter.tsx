c5dc2503868f5c4798ebfe81ee175fa1886b6ddc
biplobsec21 <biplobsec21@gmail.com>
2023-10-24 13:16:54 +0600

    sorting and filtering problem solved

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { Select } from "@radix-ui/themes"
 import { Status } from '@prisma/client'
+import { useRouter, useSearchParams } from 'next/navigation';
 const statuses: { label: string, value?: Status }[] = [
     { label: 'All' },
     { label: 'Open', value: 'OPEN' },
@@ -9,12 +10,21 @@ const statuses: { label: string, value?: Status }[] = [
     { label: 'Closed', value: 'CLOSED' },
 ]
 const IssueStatusFilter = () => {
+    const router = useRouter();
+    const searchParam = useSearchParams();
     return (
-        <Select.Root>
+        <Select.Root onValueChange={(status) => {
+            const params = new URLSearchParams();
+            if (status !== 'all') params.append('status', status);
+            if (searchParam.get('orderBy')) params.append('orderBy', searchParam.get('orderBy')!);
+
+            const queryString = params.size ? '?' + params.toString() : '';
+            router.push(`/issues/list${queryString}`);
+        }}>
             <Select.Trigger placeholder='Filter by status...' />
             <Select.Content>
                 {statuses.map((status) => (
-                    <Select.Item key={status.value} value={status.value || 'all'} >{status.label}</Select.Item>
+                    <Select.Item key={status.value || 'all'} value={status.value || 'all'} >{status.label}</Select.Item>
                 ))}
             </Select.Content>
         </Select.Root>
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,20 +1,51 @@
 import prisma from '@/prisma/client'
 import { Table } from '@radix-ui/themes'
 import { IssueStatusBadge, Link } from '@/app/components';
+import NextLink from 'next/link'
 import IssueActions from './IssueActions'
+import { Issue, Status } from '@prisma/client';
+import { ArrowUpIcon } from '@radix-ui/react-icons';
+interface Props {
+  searchParams: { status: Status, orderBy: keyof Issue }
+}
+const IssuesPage = async ({ searchParams }: Props) => {
 
-const IssuesPage = async () => {
+  const column: {
+    label: string;
+    value: keyof Issue;
+    classname?: string
+  }[] = [
+      { label: 'Issue', value: 'title' },
+      { label: 'Status', value: 'status', classname: 'hidden md:table-cell' },
+      { label: 'Date', value: 'created_at', classname: 'hidden md:table-cell' }
+    ]
 
-  const issues = await prisma.issue.findMany();
+  const allStatus = Object.values(Status);
+  const status = allStatus.includes(searchParams.status) ? searchParams.status : undefined;
+
+  const orderBy = column.map((col) => col.value).includes(searchParams.orderBy) ? { [searchParams.orderBy]: 'asc' } : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: { status: status },
+    orderBy
+  });
   return (
     <div>
       <IssueActions />
       <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+            {column.map((col) => (
+              <Table.ColumnHeaderCell key={col.value} className={col.classname}>
+                <NextLink href={{
+                  query: { ...searchParams, orderBy: col.value }
+                }}>
+                  {col.label}
+                </NextLink>
+                {col.value === searchParams.orderBy && <ArrowUpIcon className="inline" />}
+              </Table.ColumnHeaderCell>
+            ))}
+
           </Table.Row>
         </Table.Header>
         <Table.Body>
