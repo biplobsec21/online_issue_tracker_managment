a68bc009d6a0a67d89074f78744259866984c173
biplobsec21 <biplobsec21@gmail.com>
2023-10-25 14:26:11 +0600

    Issue summary done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
-export default function Home() {
+export default async function Home() {
+
+  const open = await prisma.issue.count({
+    where: { status: 'OPEN' }
+  });
+  const closed = await prisma.issue.count({
+    where: { status: 'CLOSED' }
+  });
+  const inprogress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' }
+  });
   return (
-    <LatestIssues />
+    // <LatestIssues />
+    <IssueSummary open={open} closed={closed} inprogress={inprogress} />
   )
 }
