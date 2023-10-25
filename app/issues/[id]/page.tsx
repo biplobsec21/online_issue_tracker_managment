b9a858ba311dc20ddc9dc6792d5095efa2c73343
biplobsec21 <biplobsec21@gmail.com>
2023-10-25 16:36:18 +0600

    cacheing done

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -8,15 +8,19 @@ import { getServerSession } from 'next-auth';
 import authOptions from '@/app/auth/authOptions';
 import AssigneeSelect from './AssigneeSelect';
 import { Metadata } from 'next';
+import { cache } from 'react';
 interface Props {
   params: { id: string }
 }
 
+const issueDetials = cache((issueId: number) => {
+  return prisma.issue.findUnique({
+    where: { id: (issueId) }
+  });
+})
 const IssueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions)
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) }
-  });
+  const issue = await issueDetials(parseInt(params.id));
   if (!issue)
     notFound();
 
@@ -40,9 +44,7 @@ const IssueDetailPage = async ({ params }: Props) => {
 export default IssueDetailPage
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) }
-  })
+  const issue = await issueDetials(parseInt(params.id))
   return {
     title: issue?.title,
     description: issue?.description
